00797c8e53386253412960a4e822cee6d572595f
aschahal <aschahal1234@gmail.com>
2024-01-20 15:59:25 -0600

    added channel name

--- src/HashtableMap.java
+++ src/HashtableMap.java
@@ -1,30 +1,3 @@
-//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
-//
-// Title:    P3 Hashtable Map
-// Course:   CS 400 Summer 2023
-//
-// Author:   Amandeep Chahal
-// Email:    aschahal@wisc.edu
-// Lecturer: Peyman Morteza
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:
-// Partner Email:
-// Partner Lecturer's Name:
-//
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
-//
-// Persons:         (identify each by name and describe how they helped)
-// Online Sources:  (identify each by URL and describe how it helped)
-//
-///////////////////////////////////////////////////////////////////////////////
-
 import java.util.NoSuchElementException;
 
 /**
